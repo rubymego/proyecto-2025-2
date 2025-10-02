6f5276c06e2a753bbbcb8f344bef17e335d0c9b5
Mego <sairarubymegoburgos@gmail.com>
2025-10-02 10:45:07 -0500

    Arhivo vista de bienvenido

---
+++ resources/views/bienvenidos.blade.php
@@ -0,0 +1,1 @@
+<h1>Hola my friends</h1>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/saludos',  function () {
+    return 'Hola Mundo';
+});
+
+Route::get('/bienvenidos',  function () {
+    return view ('bienvenidos');
+});
